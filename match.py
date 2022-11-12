8b0cdc6f586e412aae513434afc3827f820eafcf
joe052 <joekaranjasenior52@gmail.com>
2022-11-13 02:47:58 +0300

    message

--- match.py
+++ match.py
@@ -1,24 +1,30 @@
-boxGroups = [[140,120,150,100,170,200,90,180],[170,150,140,90,100,120,180,200],[120,90,200,150,180,140,100,170]]
+boxGroups = [[140, 120, 150, 100, 170, 200, 90, 180], [170, 150, 140,90, 100, 120, 180, 200], [120, 90, 200, 150, 180, 140, 100, 170]]
 
-def matchBox(arrays):
-    for i in range(len(arrays[0])):        
+# function to match the boxes
+def matchBoxes(arrays):
+    # picking each number at a time
+    for i in range(len(arrays[0])):
         target = arrays[0][i]
         box = 0
+        # traversing each array in the arrays
         for array in arrays:
-            if(box == 0):
+            if (box == 0):
                 boxName = 'A'
-            elif(box == 1):
+            elif (box == 1):
                 boxName = 'B'
-            elif(box == 2):
-                boxName = 'C'      
-            for i in range(len(array)):                 
-                if(array[i] == target):
+            elif (box == 2):
+                boxName = 'C'
+            # looping each element in the array
+            for i in range(len(array)):
+                # match the box id in every array we find the box with our box names
+                if (array[i] == target):
                     start = ''
-                    if(box != 0 & box != 1):                       
-                        start = ' with ' 
-                        print(start+boxName+'['+str(i+1)+']', end = "")
+                    if (box != 0 & box != 1):
+                        start = ' with '
+                        print(start+boxName+'['+str(i+1)+']', end="")
                     else:
-                        print('\n',start+boxName+'['+str(i+1)+']', end = "")                    
+                        print('\n', start+boxName+'['+str(i+1)+']', end="")
             box += 1
-            
-matchBox(boxGroups)
+
+
+matchBoxes(boxGroups)
