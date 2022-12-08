83169844e1f18ae8459252f211d243a240c35bea
Victor Jotham Ashioya <victorashioya960@gmail.com>
2022-12-08 12:59:23 +0300

    Update ValidParantheses.py

--- Turing_Tests/ValidParantheses.py
+++ Turing_Tests/ValidParantheses.py
@@ -1,20 +1,18 @@
 class Solution:
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if len(s) < 2 or len(s) % 2 != 0: return False
-        pars = {"(":")",
-                "{":"}",
-                "[":"]"}
-        if s[0] not in pars: return False
-        to_close = [pars[s[0]]]
-        for i in range(1,len(s)):
-            if s[i] in pars: 
-                to_close.append(pars[s[i]])
-            elif s[i] == to_close[-1]:
-                to_close.pop()
+        dct = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        l = len(s)
+        if l == 0:
+            return True
+        if l % 2 != 0:
+            return False
+        for num, char in enumerate(s):
+            if char in dct:
+                stack.append(dct[char])
+            elif stack and char == stack[-1]:
+                stack.pop(-1)
             else:
                 return False
-        return to_close == []
+        return not stack
+´´´
