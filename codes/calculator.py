fd1cfaaa7d41514a2e2f1307bf91e15376e8309f
joe052 <joekaranjasenior52@gmail.com>
2025-03-01 17:43:17 +0300

    message

---
+++ codes/calculator.py
@@ -0,0 +1,26 @@
+# Create a function for addition
+def addNums(x, y):
+    return x + y
+
+
+#  Create a function for addition
+def mltNums(x, y):
+    return x * y
+
+
+#  Create a function for addition
+def subNums(x, y):
+    return x - y
+
+
+#  Create a function for addition
+def divNums(x, y):
+    return x / y
+
+
+a = 15
+b = 5
+
+result = addNums(a, b)
+
+print(result)
