c833346ef8ea7c9fa1ab498d165687439857d452
joe052 <joekaranjasenior52@gmail.com>
2025-03-29 17:21:12 +0300

    reviewed

--- quizzes/Q2A.py
+++ quizzes/Q2A.py
@@ -1,12 +1,27 @@
-num1 = input("Enter first number")
-num2 = input("Enter second number")
+# PROMPT USER INPUT
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
 
+# CHECK TYPE
+print(type(num1))
+
+# convert to integer
+num1, num2 = int(num1), int(num2)
+
+# SUM
 sum = num1 + num2
-difference = num1-num2
-product = num1 * num2
-quotient = num1 / num2
 
-Print ("sum is :, sum")
-Print ("difference is:", difference)
-Print ("product is:", product)
-Print ("quotient is:" , quotient )
+# DIFFERENCE
+difference = num1 - num2
+
+# PRODUCT
+product = num1 * num2
+
+# QUOTIENT
+quotient = num1 // num2
+
+# PRINT RESULTS
+print("sum is :", sum)
+print("difference is:", difference)
+print("product is:", product)
+print("quotient is:", quotient)
---
+++ quizzes/test.java
@@ -0,0 +1,8 @@
+public class Main{
+    String name;
+
+    public static void main(string args []){
+        this.name = 5; //should throw error
+        System.out.prontLn("Bugs for the Banshee");
+    }
+}
