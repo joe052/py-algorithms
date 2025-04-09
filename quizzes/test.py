75844fe5a0b792fc4b9e01e5f8714ae8944e246a
joe052 <joekaranjasenior52@gmail.com>
2025-04-09 23:09:03 +0300

    message

---
+++ quizzes/test.py
@@ -0,0 +1,16 @@
+happy = True
+user = "staffrr"
+
+# if happy:
+#     print("I will take you out!")
+# else:
+#     print("Go sleep!")
+
+if user == "manager":
+    print("This is a manager")
+elif user == "staff":
+    print("This is staff")
+elif user == "customer":
+    print("This is customer")
+else:
+    print("Who the hell are you?")
